36f48c0cbfa35432b317ec3e850039da7a6f1846
gemaskus <gemaskus@icloud.com>
2024-08-08 00:31:48 -0600

    laid the ground work for the maze class with the function defs and the like

---
+++ maze.py
@@ -0,0 +1,26 @@
+from cell import Cell
+from graphics import Line, Point
+
+
+class Maze():
+    def __init__(self,
+                 x1,
+                 y1,
+                 num_rows,
+                 num_cols,
+                 cell_size_x,
+                 cell_size_y,
+                 win):
+        pass
+
+    def _create_cells(self):
+        pass
+
+    def _draw_cell(self, i, j):
+        pass
+
+    def _animate(self):
+        pass
+
+
+
